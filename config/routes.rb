35282cdabd1cd72ce93e36b56ffe4eb67fc42823
Guilhem Romo <guilhem.romo@orange.fr>
2023-04-26 06:58:38 +0200

    les 3 premieres pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+  get "home", to: "pages#home"
+  get "index", to: "pages#index"
+  get "morpion", to: "pages#morpion"
 end
