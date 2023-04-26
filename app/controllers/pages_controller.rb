0c02ec0ba7c9f80e702fc305cc734bcae08cf4e3
Guilhem Romo <guilhem.romo@orange.fr>
2023-04-26 08:47:10 +0200

    pages Home Index Morpio et les liens

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,9 @@ class PagesController < ApplicationController
   end
 
   def index
+    @games = ["Morpio, c'est comme un morpion mais sans le n", "Le Sssssnake", "La bataille corse, mais sans se niquer les mains", "Le démineur niveau facile pcq flemme de faire plus", "Et pleins d'autres quand j'aurai des idées!"]
   end
 
-  def morpion
+  def morpio
   end
 end
