963c69eed4a47695be11d0da614718bd8864b4ad
Guilhem Romo <guilhem.romo@orange.fr>
2023-04-28 02:57:01 +0200

    fin de la premiere mise en page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   end
 
   def index
-    @games = ["Morpio, c'est comme un morpion mais sans le n", "Le Sssssnake", "La bataille corse, mais sans se niquer les mains", "Le démineur niveau facile pcq flemme de faire plus", "Et pleins d'autres quand j'aurai des idées!", "Une dernière card pour la grid"]
+    @games = ["Morpio, c'est comme un morpion mais sans le n", "Le Sssssnake", "La bataille corse, mais sans se niquer les mains", "Le démineur niveau facile pcq flemme de faire plus", "Et pleins d'autres quand j'aurai des idées!"]
   end
 
   def morpio
